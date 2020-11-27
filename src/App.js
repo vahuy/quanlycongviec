4f48887cea772b98fe1df378ab4699c6e01de652
Huy <anhuy121@gmail.com>
2020-11-27 11:34:55 +0700

    [update] create ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import './App.css';
-import React from "react";
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <i className="fa fa-surprise">Here</i>
-        <p>
-          Edit <code>
-          src/App.js
-        </code> and save to reload.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div>
+                    <h1>Quan ly cong viec</h1>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    {/*Them/Sua*/}
+                        <div className="panel panel-default">
+                            <div className="panel-body">
+                                them cong viec
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                        <div className="panel panel-default">
+                            <div className="panel-body">
+                                <div><button type="submit">Them cong viec</button></div>
+                                <div className="form-group">
+                                    <div className="col-sm-10">
+                                        <input type="text" name="name" id="inputID" className="form-control" value=""
+                                               title="" required="required"/><button type="submit">Tim</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>aaa<i className="fab fa-500px"/></div>
+                <div>bbb<i className="fab fa-accessible-icon"/></div>
+                <div>ccc<i className="fas fa-ambulance"/></div>
+                <div>ddd<i className="fas fa-ankh"/></div>
+                <div>eee<i className="fas fa-laptop-code"/></div>
+            </div>
+        );
+    }
 }
 
 export default App;
